7ec623daf38b6854dbbf7b9f462b8855224c4b10
Ba Mang <76155923+mangaaaa@users.noreply.github.com>
2021-03-31 11:02:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     $data = pg_query($dbconn,$sql); 
     $login_check = pg_num_rows($data);
     if($login_check > 0){ 
-        
-        echo "Login Successfully";    
+        header('location: a.php');   
     }else{
         
         echo "Invalid Details";
