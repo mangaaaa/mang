1069d9d054f19ab0fa0e751620003b58d12b2d2f
Ba Mang <76155923+mangaaaa@users.noreply.github.com>
2021-03-31 10:37:07 +0100

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,10 +1,16 @@
 <?php 
 session_start();
+$dbConn = pg_connect("host=<
+ec2-23-22-191-232.compute-1.amazonaws.com> port=<5432> dbname=<d9kre3i7166g8k> user=<gmpgirnharzwij> password=<9747f761fd35e62bfb0e8db8b073598e6f674ee9ae63563a4b6c00ea3e9e11e0>");
+if (!$dbConn) {
+    echo "An error occurred.\n";
+    exit;
+}
 if($_SERVER['REQUEST_METHOD']=='POST'){
 	include('conn.php');
 	$username=$_POST['name'];
 	$password=$_POST['pass'];
-	$user= pg_query($conn, "SELECT * FROM login WHERE us='{$username}' AND pw='{$password}'"));
+	$user= pg_query($dbConn, "SELECT * FROM login WHERE us='{$username}' AND pw='{$password}'"));
 	if ($user) {
 		$_SESSION['user']=$user['username'];
 		header('location: index.php');
