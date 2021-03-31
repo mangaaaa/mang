9ab5d5ee44fd81b7f953296547c1425b19c7e2cc
Ba Mang <76155923+mangaaaa@users.noreply.github.com>
2021-03-31 11:11:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 $dbConn = pg_connect("host=
 ec2-23-22-191-232.compute-1.amazonaws.com port=5432 dbname=d9kre3i7166g8k user=gmpgirnharzwij password=9747f761fd35e62bfb0e8db8b073598e6f674ee9ae63563a4b6c00ea3e9e11e0");
-if (!$dbConn) {
+if (!$dbconn) {
     echo "An error occurred.\n";
     exit;
 }
