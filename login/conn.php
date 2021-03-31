2688e85d20a4496068c5ce45260d0358dfee0072
Ba Mang <76155923+mangaaaa@users.noreply.github.com>
2021-03-31 10:31:20 +0100

    Update conn.php

--- login/conn.php
+++ login/conn.php
@@ -4,8 +4,9 @@ ec2-23-22-191-232.compute-1.amazonaws.com";
 $username="gmpgirnharzwij";
 $password="9747f761fd35e62bfb0e8db8b073598e6f674ee9ae63563a4b6c00ea3e9e11e0";
 $dbname="d9kre3i7166g8k";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+$conn=pg_connect($servername,$username,$password,$dbname);
 if (!$conn) {
-	die("Error Connection: ".mysqli_connect_error());
+	echo "An error occurred.\n";
+    exit;
 } 
  ?>
